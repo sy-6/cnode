3275635b14532aa2f62afd949aa0554cb5cf49c2
sy-6 <249859693@qq.com>
2021-07-14 12:14:44 +0800

    Delete src directory

--- src/plugin/axios.js
+++
@@ -1,8 +0,0 @@
-import axios from 'axios'
-import Vue from 'vue'
-axios.defaults.baseURL = 'https://cnodejs.org/api/v1'
-axios.interceptors.response.use(res => {
-    return res.data
-})
-Vue.prototype.$axios = axios
-export default axios
